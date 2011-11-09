9fe39dce7b341daa10399b90105c0185d70fcef5
Ryan Smith <gfistainlessdev@gmail.com>
2011-11-09 09:54:08 -0800

    set up factories

--- app/models/productcategory.rb
+++ app/models/productcategory.rb
@@ -1,3 +1,4 @@
 class Productcategory < ActiveRecord::Base
+  has_ancestry
   validates :name, presence: true, length: {maximum:250}
 end
--- spec/factories/productcategories.rb
+++ spec/factories/productcategories.rb
@@ -1,8 +1,9 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :productcategory do
-      name "MyString"
-      ancestry "MyString"
-    end
+Factory.define(:productcategory, class: Productcategory) do |c|
+  c.sequence(:name) { |n| Faker::Lorem.words(rand(3) + 1).join(' ') + n.to_s}
+end
+
+Factory.define :product_category_with_parent, parent: :productcategory do |f|
+  f.parent {Factory.create(:productcategory)}
 end
