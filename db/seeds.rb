1d6a482c3121b97b7df31cb421463b19dea10795
Ryan Smith <gfistainlessdev@gmail.com>
2011-11-11 13:51:55 -0800

    More real seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,17 +26,36 @@ user.admin=true
 user.save!
 #------End users
 #------Categories
-top=Productcategory.find_or_initialize_by_name("TopLevel")
-top.save!
-firstchild=Productcategory.find_or_initialize_by_name("FirstChild")
-firstchild.parent=top
-firstchild.save!
-secondchild=Productcategory.find_or_initialize_by_name("SecondChild")
-secondchild.parent=top
-secondchild.save!
+#top=Productcategory.find_or_initialize_by_name("TopLevel")
+#top.save!
+#firstchild=Productcategory.find_or_initialize_by_name("FirstChild")
+#firstchild.parent=top
+#firstchild.save!
+#secondchild=Productcategory.find_or_initialize_by_name("SecondChild")
+#secondchild.parent=top
+#secondchild.save!
 #-----End Categories
 #-----Products
-firstproduct=Product.find_or_create_by_name("Product1")
-firstproduct.productcategory=firstchild
-firstproduct.save!
+#firstproduct=Product.find_or_create_by_name("Product1")
+#firstproduct.productcategory=firstchild
+#firstproduct.save!
 
+#---Categories2
+def process_category_node(node, parent = nil)
+  name, children = case node
+                  when Hash
+                    [node.keys.first, node.values.first]
+                  else
+                    [node, []]
+                  end
+  cat = if parent
+          Productcategory.children_of(parent.id).find_or_create_by_name(name)
+        else
+          Productcategory.find_or_create_by_name(name)
+        end
+  children.each {|c| process_category_node(c,cat)}
+end
+Rails.root.join('db', 'categories.yml').open do |file|
+  categories_hash= YAML.load(file)
+  categories_hash.each {|c| process_category_node(c)}
+end
